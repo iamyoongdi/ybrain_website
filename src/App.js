686fe2eded5ede469fa93d31aece127da131ae0e
Yoonji Lee <iamyoongdi@gmail.com>
2017-06-17 22:58:47 +0900

    change src folders

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
-import React, { Component } from '../node_modules/react';
-import ImageGallery from 'react-image-gallery';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
-import "./image-gallery.css";
+import Home from './components/home/home.js'
 
 class App extends Component {
-  handleImageLoad(event) {
-    console.log('Image loaded ', event.target)
-  }
+
   render() {
-    const images = [
-      {
-        original: 'http://lorempixel.com/1000/600/nature/1/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/1/',
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/2/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/2/'
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/3/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/3/'
-      }
-    ]
+    
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<h2>Welcome to React</h2>*/}
         </div>
-        <ImageGallery
-        items={images}
-        slideInterval={4000}
-        showThumbnails={false}
-        showBullets={true}
-        showFullscreenButton={false}
-        showPlayButton={false}
-        onImageLoad={this.handleImageLoad}/>
+        <Home/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/home/home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery';
+import './home.css';
+import "./image-gallery.css";
+
+class Home extends Component {
+  handleImageLoad(event) {
+    console.log('Image loaded ', event.target)
+  }
+  render() {
+    const images = [
+      {
+        original: require('../../assets/landing_1.png'),
+      },
+      {
+        original: require('../../assets/landing_2.png'),
+      },
+      {
+        original: require('../../assets/landing_3.png'),
+      }
+    ]
+    return (
+      <div className="container">
+          <ImageGallery
+            items={images}
+            slideInterval={4000}
+            showThumbnails={false}
+            showBullets={true}
+            showFullscreenButton={false}
+            showPlayButton={false}
+            onImageLoad={this.handleImageLoad}/>
+    </div>
+    );
+  }
+}
+
+export default Home;
