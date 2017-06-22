90b0576e92d303714f65da651d49d69d317750d0
Yangwoo Lee <lyw0149@gmail.com>
2017-06-23 00:22:49 +0900

    use serve for deploy

---
+++ src/components/CommonContact/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class CommonContact extends Component {
+
+  render() {
+    return (
+    <div className="common-contact">
+
+    </div>
+
+    );
+  }
+}
+
+export default CommonContact;
