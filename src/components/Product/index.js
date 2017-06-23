0663a0c96d6ecf057f113adacfd69f6b1f2bb503
Yangwoo Lee <lyw0149@gmail.com>
2017-06-24 04:20:53 +0900

    Responsive (#8)
    * responsive
    
    * responsive & mobile header.

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,28 +1,111 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Responsive from 'react-responsive'
 
-import './style.css';
-import CommonContact from '../CommonContact';
+import './style.css'
+import CommonContact from '../CommonContact'
 
-import mainImage from '../../assets/slideimgs-product@2x.png';
-import sylbolImage from '../../assets/ybrain-symbol@3x.png';
+import mainImage from '../../assets/slideimgs-product.png'
+import mainImageMobile from '../../assets/slideimgs-product-mobile@2x.png'
+import symbolImage from '../../assets/ybrain-symbol@3x.png'
 
-class Product extends Component {
+const Desktop = ({ children }) => <Responsive minWidth={768} children={children} />
+const Mobile = ({ children }) => <Responsive maxWidth={768} children={children} />
 
-  render() {
+class Purpose extends Component {
+  render () {
     return (
       <div className='inner-container'>
-        <img className='main-img' alt='' src={mainImage} />
+        <Desktop>
+          <img className='main-img' alt='' src={mainImage} />
+        </Desktop>
+        <Mobile>
+          <img className='main-img' alt='' src={mainImageMobile} />
+        </Mobile>
         <div className='main-container'>
           <div className='main-container-header'>
-            <img className='main-container-symbol' alt='' src={sylbolImage} />
-            <h2 className='main-container-title'>Product</h2>
+            <img className='main-container-symbol' alt='' src={symbolImage} />
+            <h2 className='main-container-title'>Purpose</h2>
           </div>
-          <div className='main-container-content'/>
+          <div className='main-container-content' />
         </div>
         <CommonContact />
       </div>
-    );
+    )
   }
 }
 
-export default Product;
+class Features extends Component {
+  render () {
+    return (
+      <div className='inner-container'>
+        <Desktop>
+          <img className='main-img' alt='' src={mainImage} />
+        </Desktop>
+        <Mobile>
+          <img className='main-img' alt='' src={mainImageMobile} />
+        </Mobile>
+        <div className='main-container'>
+          <div className='main-container-header'>
+            <img className='main-container-symbol' alt='' src={symbolImage} />
+            <h2 className='main-container-title'>Features</h2>
+          </div>
+          <div className='main-container-content' />
+        </div>
+        <CommonContact />
+      </div>
+    )
+  }
+}
+
+class Es extends Component {
+  render () {
+    return (
+      <div className='inner-container'>
+        <Desktop>
+          <img className='main-img' alt='' src={mainImage} />
+        </Desktop>
+        <Mobile>
+          <img className='main-img' alt='' src={mainImageMobile} />
+        </Mobile>
+        <div className='main-container'>
+          <div className='main-container-header'>
+            <img className='main-container-symbol' alt='' src={symbolImage} />
+            <h2 className='main-container-title'>Efficacy & Safety</h2>
+          </div>
+          <div className='main-container-content' />
+        </div>
+        <CommonContact />
+      </div>
+    )
+  }
+}
+
+class Use extends Component {
+  render () {
+    return (
+      <div className='inner-container'>
+        <Desktop>
+          <img className='main-img' alt='' src={mainImage} />
+        </Desktop>
+        <Mobile>
+          <img className='main-img' alt='' src={mainImageMobile} />
+        </Mobile>
+        <div className='main-container'>
+          <div className='main-container-header'>
+            <img className='main-container-symbol' alt='' src={symbolImage} />
+            <h2 className='main-container-title'>Use</h2>
+          </div>
+          <div className='main-container-content' />
+        </div>
+        <CommonContact />
+      </div>
+    )
+  }
+}
+
+export {
+  Purpose,
+  Features,
+  Es,
+  Use
+}
